7990d6912112b2bd5653810824c2de6f6e2f78c7
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-05-02 12:22:30 +0330

    Feat(gameChallenge):set parent element on body
    set parent div on body. add style and basic structure for parentDiv.

---
+++ gameChalleng/game.js
@@ -0,0 +1,18 @@
+import domGenerator from "dom-generator";
+import "./index.scss";
+
+/**
+ * Generates a button that changes the background color of the body in a cyclic order when clicked.
+ * @returns {void}
+ */
+function gameChallenge() {
+  let game = domGenerator({
+    tag: "div",
+    attributes: { id: "parentDiv" },
+  });
+
+
+
+  document.body.append(game);
+}
+export default gameChallenge;
--- main.js
+++ main.js
@@ -2,24 +2,29 @@ import changeColorOrder from "./DiscoloredInOrder/DiscoloredOrder";
 import temperatureConvert from "./convertCtoF/convert";
 import createColors from "./createColor/createColor";
 import changeColor from "./ChangeColor/changeColor";
+import gameChallenge from "./gameChalleng/game";
 
 /**
  * Prompts the user to choose a project and executes the corresponding function based on the choice.
  * @returns {void}
  */
-let nameProject = prompt(`Which one project choose:
-1. Change the color of the plate between two specific colors
-2. Display the color with a special arrangement
-3. Construction of color with RGB
-4. temperatureConvert
-`);
+// let nameProject = prompt(`Which one project choose:
+// 1. Change the color of the plate between two specific colors
+// 2. Display the color with a special arrangement
+// 3. Construction of color with RGB
+// 4. temperatureConvert
+// `);
 
-if (Number(nameProject) === 1) {
-  changeColor();
-} else if (Number(nameProject) === 2) {
-  changeColorOrder();
-} else if (Number(nameProject) === 3) {
-  createColors();
-} else if (Number(nameProject) === 4) {
-  temperatureConvert();
-}
+// if (Number(nameProject) === 1) {
+//   changeColor();
+// } else if (Number(nameProject) === 2) {
+//   changeColorOrder();
+// } else if (Number(nameProject) === 3) {
+//   createColors();
+// } else if (Number(nameProject) === 4) {
+//   temperatureConvert();
+// }
+// else if (Number(nameProject) === 5) {
+//   gameChallenge()
+// }
+gameChallenge()
