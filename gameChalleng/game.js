2243c0864cfd74af1dc368c7cfc461509a603d6f
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-05-02 14:51:32 +0330

    Feat(gameChallenge):basic styles
    add styles for all elements on game.
    add font.

--- gameChalleng/game.js
+++ gameChalleng/game.js
@@ -9,9 +9,58 @@ function gameChallenge() {
   let game = domGenerator({
     tag: "div",
     attributes: { id: "parentDiv" },
+    children: [
+      {
+        tag: "div",
+        attributes: { id: "gamePlace" },
+        children: [
+          {
+            tag: "h1",
+            attributes: { id: "title" },
+            properties: { textContent: "Welcome in my Game" },
+          },
+          {
+            tag: "p",
+            attributes: { id: "paragraph" },
+            properties: {
+              textContent:
+                "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt provident tenetur, laboriosam soluta, dolorem magnam rerum quia distinctio dignissimos possimus corporis quos, quo quisquam? Id ducimus consequatur quas nobis iusto. ",
+            },
+          },
+          {
+            tag: "form",
+            attributes: { id: "form" },
+            children: [
+              {
+                tag: "label",
+                properties: { textContent: "this is label" },
+              },
+              {
+                tag: "input",
+                properties: { type: "text", placeholder: "input..." },
+                attributes: { id: "input" },
+              },
+              {
+                tag: "button",
+                attributes: { id: "buttonCTA" },
+                eventListeners: { click: gust },
+                properties: { textContent: "Welcome in my Game" },
+              },
+            ],
+          },
+          {
+            tag: "button",
+            attributes: { id: "buttonP" },
+            eventListeners: { click: action },
+            properties: { textContent: "START" },
+          },
+        ],
+      },
+    ],
   });
 
-
+  function gust() {}
+  function action() {}
 
   document.body.append(game);
 }
