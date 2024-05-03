baef36e092a2f6929e978e2f1191e5bd62d1237b
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-05-03 14:07:49 +0330

    Docs(findProject):set comment and description
    Complete description of how functioning and inputs work

--- src/findProject.js
+++ src/findProject.js
@@ -4,7 +4,15 @@ import createColors from "../createColor/createColor";
 import changeColor from "../ChangeColor/changeColor";
 import gameChallenge from "../gameChalleng/game";
 
+/**
+ * Searches for and executes a JavaScript function based on the provided function name.
+ * @returns {void}
+ */
 function findChallenge() {
+  /**
+   * An array containing objects with the names of JavaScript functions.
+   * @type {Array<Object>}
+   */
   let array = [
     {
       name: "changeColor",
@@ -22,7 +30,11 @@ function findChallenge() {
       name: "gameChallenge",
     },
   ];
-
+  /**
+   * Iterates over the array of function names and executes the function if a match is found.
+   * @param {string} funcName - The name of the function to search for and execute.
+   * @returns {void}
+   */
   function find(funcName) {
     array.forEach((item) => {
       if (item.name === funcName) {
@@ -30,6 +42,10 @@ function findChallenge() {
       }
     });
   }
+  /**
+   * Prompts the user to enter the name of the project they want to execute.
+   * @type {string}
+   */
   let nameProject = prompt(`Which project do you want:(Enter the project name⚠️)
             changeColor🎨
             changeColorOrder🎭
