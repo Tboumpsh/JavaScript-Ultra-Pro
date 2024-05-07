e6cd5432cae55039844ef1259a6b37eea10ebba9
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-05-07 15:42:19 +0330

    Feat(sendEmail):add input component
    create input. this function include all style and attributes for inputs.

--- sendEmail/sendEmail.js
+++ sendEmail/sendEmail.js
@@ -3,12 +3,27 @@ import "./index.scss";
 
 function sendEmail() {
   let send = domGenerator({
-    tag: "div",
-    attributes: { id: "parentDiv" },
-    // children: [{}],
+    tag: "section",
+    attributes: { id: "section" },
+    children: [
+      {
+        tag: "div",
+        attributes: { id: "parentDiv" },
+        children:[
+            {
+                tag:'form',
+                attributes:{id:'formValidation'},
+                children:[
+                    {
+                        tag:'input',
+                    }
+                ]
+            }
+        ]
+      },
+    ],
   });
 
-
-  document.body.append(send)
+  document.body.append(send);
 }
 export default sendEmail;
