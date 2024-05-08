b7f8a995f4ec8e884988a6ce277ea88cd5f44859
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-05-08 16:22:02 +0330

    Docs(sendEmail):sendEmail.js
    Full description of how the function of the function and its inputs and the amount returned by JSDOCS.

--- sendEmail/sendEmail.js
+++ sendEmail/sendEmail.js
@@ -3,6 +3,10 @@ import "./index.scss";
 
 import formGenerator from "./formGenerator";
 
+/**
+ * Generates and appends a form to the document body for sending an email.
+ *  @returns {void}
+ */
 function sendEmail() {
   let send = domGenerator({
     tag: "section",
