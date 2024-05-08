adaebb54dbc764e38864068d65b8988b795909fc
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-05-08 16:19:04 +0330

    Refactor(sendEmail):formGenerator
    Construction and validation of each form was separated. Each input carries out the amount by input by entering some of it.

---
+++ sendEmail/formGenerator.js
@@ -0,0 +1,143 @@
+import domGenerator from "dom-generator";
+import "./index.scss";
+
+import buttonGenerator from "./components/buttonComponents/buttonComponent";
+import inputGenerator from "./components/inputComponents/inputComponent";
+
+function formGenerator() {
+  let form = domGenerator({
+    tag: "form",
+    attributes: { id: "formValidation" },
+    eventListeners: { submit: (e) => e.preventDefault() },
+    children: [
+      {
+        tag: inputGenerator({
+          inputId: "inputEmail",
+          placeholder: "email",
+          type: "email",
+          fontSize: "medium",
+          size: "large",
+          statues: "mainInput",
+          eventListeners: { blur: checkEmail },
+        }),
+      },
+      {
+        tag: inputGenerator({
+          inputId: "inputSubject",
+          placeholder: "subject",
+          type: "text",
+          fontSize: "medium",
+          size: "large",
+          statues: "mainInput",
+          eventListeners: { blur: checkSubject },
+        }),
+      },
+      {
+        tag: inputGenerator({
+          inputId: "inputMessage",
+          placeholder: "message",
+          type: "text",
+          fontSize: "medium",
+          size: "large",
+          statues: "mainInput",
+          eventListeners: { blur: checkMessage },
+        }),
+      },
+      {
+        tag: "div",
+        attributes: { id: "buttonsSection" },
+        children: [
+          {
+            tag: buttonGenerator({
+              content: "sendEmail",
+              size: "medium",
+              statues: "primary",
+              type: "button",
+              class: "buttonPrimary",
+              eventListeners: { click: sendEmails },
+            }),
+          },
+          {
+            tag: buttonGenerator({
+              content: "Refresh",
+              size: "medium",
+              statues: "Refresh",
+              type: "reset",
+              class: "buttonRefresh",
+              eventListeners: { click: RefreshEmails },
+            }),
+          },
+        ],
+      },
+    ],
+  });
+
+  let count = 0;
+
+  function checkEmail() {
+    let inputEmail = document.getElementById("inputEmail");
+    let type = inputEmail.type;
+    if (type === "email") {
+      if (inputEmail.value !== "") {
+        if (inputEmail.value.includes("@")) {
+          inputEmail.setAttribute("data-status", "checkedInput");
+          count++;
+        } else {
+          inputEmail.setAttribute("data-status", "failedInput");
+          count--;
+        }
+      } else {
+        inputEmail.setAttribute("data-status", "failedInput");
+        alert("Please enter your email");
+      }
+    }
+  }
+
+  function checkSubject() {
+    let inputSubject = document.getElementById("inputSubject");
+    let type = inputSubject.type;
+    if (type === "text") {
+      if (inputSubject.value !== "") {
+        inputSubject.setAttribute("data-status", "checkedInput");
+        count++;
+      } else {
+        inputSubject.setAttribute("data-status", "failedInput");
+        inputSubject.value = "undefined";
+        count--;
+      }
+    }
+  }
+
+  function checkMessage() {
+    let inputMessage = document.getElementById("inputMessage");
+    let type = inputMessage.type;
+    if (type === "text") {
+      if (inputMessage.value !== "") {
+        inputMessage.setAttribute("data-status", "checkedInput");
+        count++;
+      } else {
+        inputMessage.setAttribute("data-status", "failedInput");
+        count--;
+        alert("Please enter yor message");
+      }
+    }
+  }
+  function sendEmails() {
+    console.log(count);
+    if (count == 3) {
+      alert("send");
+    } else {
+      alert("not send");
+    }
+  }
+  function RefreshEmails() {
+    let inputMessage = document.getElementById("inputMessage");
+    let inputSubject = document.getElementById("inputSubject");
+    let inputEmail = document.getElementById("inputEmail");
+    inputEmail.value = "";
+    inputSubject.value = "";
+    inputMessage.value = "";
+  }
+  return form;
+}
+export default formGenerator;
