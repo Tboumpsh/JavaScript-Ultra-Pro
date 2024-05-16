03883c49e683d17afce0e6892f91fb00f780f562
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-05-16 17:56:46 +0330

    Docs(checkLength): set JsDoc for function
    Complete explanations about how each function works and the various inputs that are included.

--- TypeScriptSendEmail/checkLength.ts
+++ TypeScriptSendEmail/checkLength.ts
@@ -1,11 +1,34 @@
 import success from "./helpers/checkedValidation";
 import failed from "./helpers/failedValidation";
 
-// check length
+/**
+ * Validates the length of an input's value and updates its status.
+ *
+ * This function checks whether the value of the provided input element has a length greater than 0.
+ * If the length is greater than 0, it calls the `success` function with the input as the argument.
+ * Otherwise, it calls the `failed` function with the input as the argument.
+ *
+ * @function checkLength
+ * @param {HTMLInputElement} input - The input element to be validated.
+ * @returns {void}
+ */
+
 function checkLength(input: any): void {
   if (input.value.length > 0) {
+    /**
+     * Marks the input as successful.
+     *
+     * @param {HTMLInputElement} input - The input element to be marked as successful.
+     * @returns {void}
+     */
     success(input);
   } else {
+    /**
+     * Marks the input as failed.
+     *
+     * @param {HTMLInputElement} input - The input element to be marked as failed.
+     * @returns {void}
+     */
     failed(input);
   }
 }
