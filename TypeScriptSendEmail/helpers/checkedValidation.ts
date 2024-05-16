f706326fcb8a4c1be01aafa3fbe290d7af7d06c6
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-05-16 18:13:19 +0330

    Docs(checkedValidation.ts):set JsDocs function
    Complete descriptions of how the function and the various inputs are included. It also specifies the typing of each function and its outputs.

--- TypeScriptSendEmail/helpers/checkedValidation.ts
+++ TypeScriptSendEmail/helpers/checkedValidation.ts
@@ -1,3 +1,14 @@
+/**
+ * Marks the input as successful.
+ *
+ * This function sets the CSS class of the provided input element to "checked",
+ * indicating that the input has passed validation or completed successfully.
+ *
+ * @function success
+ * @param {HTMLElement} input - The input element to be marked as successful.
+ * @returns {void}
+ */
+
 function success(input: any): void {
   input.setAttribute("class", "checked");
 }
