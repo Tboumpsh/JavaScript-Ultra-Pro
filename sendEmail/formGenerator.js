74e2ed2a1b085b952d4a3cb33385e5dcd75f972b
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-05-16 17:44:26 +0330

    Refactor(sendEmail):send email button
    The previous piece of code was not dynamic because it used a global variable and caused problems.
    And if the number of inputs were low or high, it would be a problem

--- sendEmail/formGenerator.js
+++ sendEmail/formGenerator.js
@@ -87,8 +87,6 @@ function formGenerator() {
     ],
   });
 
-  let count = 0;
-
   function checkEmail() {
     let inputEmail = document.getElementById("inputEmail");
     let type = inputEmail.type;
@@ -99,15 +97,12 @@ function formGenerator() {
           inputEmail.value.indexOf(".") !== -1
         ) {
           inputEmail.setAttribute("data-status", "checkedInput");
-          inputEmail.setAttribute("data-check", "check");
-          count++;
+          inputEmail.classList.add("check");
         } else {
           inputEmail.setAttribute("data-status", "failedInput");
-          count--;
         }
       } else {
         inputEmail.setAttribute("data-status", "failedInput");
-        count--;
         silverBox({
           position: "top-right",
           alertIcon: "info",
@@ -125,12 +120,10 @@ function formGenerator() {
     if (type === "text") {
       if (inputSubject.value !== "") {
         inputSubject.setAttribute("data-status", "checkedInput");
-        inputSubject.setAttribute("data-check", "check");
-        count++;
+        inputSubject.classList.add("check");
       } else {
         inputSubject.setAttribute("data-status", "failedInput");
         inputSubject.value = "undefined";
-        count--;
       }
     }
   }
@@ -141,11 +134,9 @@ function formGenerator() {
     if (type === "text") {
       if (inputMessage.value !== "") {
         inputMessage.setAttribute("data-status", "checkedInput");
-        inputMessage.setAttribute("data-check", "check");
-        count++;
+        inputMessage.classList.add("check");
       } else {
         inputMessage.setAttribute("data-status", "failedInput");
-        count--;
         silverBox({
           position: "top-right",
           alertIcon: "info",
@@ -157,12 +148,19 @@ function formGenerator() {
     }
   }
   function sendEmails() {
-    if (count == 3) {
+    let inputs = document.getElementsByTagName("input");
+    let check = document.getElementsByClassName("check");
+    if (check.length == inputs.length - 1) {
       let loading = document.getElementById("loading");
       loading.setAttribute("data-status", "load");
       setTimeout(() => {
         loading.setAttribute("data-status", "send");
       }, 3000);
+
+      setTimeout(() => {
+        loading.setAttribute("data-status", "default");
+        RefreshEmails();
+      }, 7000);
     } else {
       silverBox({
         alertIcon: "error",
@@ -173,57 +171,6 @@ function formGenerator() {
         },
       });
     }
-
-    // let inputs = document.getElementsByTagName("input");
-
-    // let loading = document.getElementById("loading");
-    // for (let i = 0; i < inputs.length; i++) {
-    //   if (!inputs[i].hasAttribute("data-check")) {
-    //     silverBox({
-    //       alertIcon: "error",
-    //       text: "Please check the entered information, you may not have a field wrong or filling out.",
-    //       centerContent: true,
-    //       cancelButton: {
-    //         text: "OK",
-    //       },
-    //     });
-    //   } else {
-    //     loading.setAttribute("data-status", "load");
-    //     setTimeout(() => {
-    //       loading.setAttribute("data-status", "send");
-    //     }, 3000);
-    //   }
-    // }
-
-    // Fixing the condition to check if an input has the "data-check" attribute
-    // let inputs = document.getElementsByTagName("input");
-    // let hasInputCount = false;
-
-    // for (let i = 0; i < inputs.length; i++) {
-    //   if (inputs[i].hasAttribute("data-check")) {
-    //     // Use inputs[i] instead of inputs.includes
-    //     hasInputCount = true; // Use true to indicate that an input has the attribute
-    //     let loading = document.getElementById("loading");
-    //     loading.setAttribute("data-status", "load");
-    //     setTimeout(() => {
-    //       loading.setAttribute("data-status", "send");
-    //     }, 3000);
-    //     break; // Exit the loop as soon as an input has the "data-check" attribute
-    //   }
-    // }
-
-    // // Fixing the condition to check if hasInputCount is true
-    // if (hasInputCount) {
-    //   // Remove unnecessary comparison with true
-    //   silverBox({
-    //     alertIcon: "error",
-    //     text: "Please check the entered information. You may have a field wrong or not filled out.",
-    //     centerContent: true,
-    //     cancelButton: {
-    //       text: "OK",
-    //     },
-    //   });
-    // }
   }
   /**
    * RefreshEmails function to reset a form and update input elements.
@@ -238,10 +185,8 @@ function formGenerator() {
    * @global
    */
   function RefreshEmails() {
-    // history.go(0);
     let form = document.getElementById("formValidation");
     form.reset();
-    count = 0;
     let inputs = document.getElementsByTagName("input");
     for (let i = 0; i < inputs.length; i++) {
       inputs[i].setAttribute("data-status", "mainInput");
