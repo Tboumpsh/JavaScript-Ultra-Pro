4686ec42e543ed0a7435c7245dcffe641d23ef65
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-05-16 18:02:22 +0330

    Docs(emailValidation): function jsDocs
    Complete descriptions of how the function and the various inputs are included. It also specifies the typing of each function and its outputs.

--- TypeScriptSendEmail/emailValidation.ts
+++ TypeScriptSendEmail/emailValidation.ts
@@ -1,8 +1,25 @@
 import success from "./helpers/checkedValidation";
 import failed from "./helpers/failedValidation";
 
+/**
+ * Validates an email input and updates its status.
+ *
+ * This function checks whether the provided input element's value meets basic email format criteria.
+ * Specifically, it verifies that the value contains an "@" symbol, a dot ("."),
+ * and the substring "com". If the value meets these criteria, the function calls
+ * the `success` function with the input as the argument. Otherwise, it calls the `failed` function with the input as the argument.
+ *
+ * @function emailValidation
+ * @param {HTMLInputElement} input - The input element to be validated.
+ * @returns {void}
+ */
+
 function emailValidation(input: any): void {
-  if (input.value.includes("@") && input.value.indexOf(".") !== -1 && input.value.indexOf("com") !== -1) {
+  if (
+    input.value.includes("@") &&
+    input.value.indexOf(".") !== -1 &&
+    input.value.indexOf("com") !== -1
+  ) {
     success(input);
   } else {
     failed(input);
