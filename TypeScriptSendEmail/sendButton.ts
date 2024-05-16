c1fe4993c3ac8ff00b01eab4aa11686f33b413d4
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-05-16 18:04:24 +0330

    Docs(sendButton):function JsDocs
    Complete descriptions of how the function and the various inputs are included. It also specifies the typing of each function and its outputs.

--- TypeScriptSendEmail/sendButton.ts
+++ TypeScriptSendEmail/sendButton.ts
@@ -1,3 +1,14 @@
+/**
+ * Updates the text and class of a submit button to indicate loading and then success.
+ *
+ * This function performs the following actions:
+ * 1. Retrieves the button element with the id "submit".
+ * 2. Changes the button's inner text to "Loading... ⏳" and sets its class to "load".
+ * 3. After a delay of 4 seconds, changes the button's inner text to "sended 📩" and sets its class to "send".
+ *
+ * @function changeButton
+ * @returns {void}
+ */
 function changeButton() {
     let send: any = document.getElementById("submit");
     send.innerText = `Loading... ⏳`;
