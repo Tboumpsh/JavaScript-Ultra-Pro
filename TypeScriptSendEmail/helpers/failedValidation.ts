0b7feb79f870a0d6a1c35b213b87e7f7779dfc89
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-05-16 18:15:28 +0330

    Docs(failedValidation.ts):set JsDocs function
    Complete descriptions of how the function and the various inputs are included. It also specifies the typing of each function and its outputs.

--- TypeScriptSendEmail/helpers/failedValidation.ts
+++ TypeScriptSendEmail/helpers/failedValidation.ts
@@ -1,3 +1,14 @@
+/**
+ * Marks the input as failed.
+ *
+ * This function sets the CSS class of the provided input element to "error",
+ * indicating that the input has failed validation or encountered an error.
+ *
+ * @function failed
+ * @param {HTMLElement} input - The input element to be marked as failed.
+ * @returns {void}
+ */
+
 function failed(input: any): void {
     input.setAttribute("class", "error");
   }
