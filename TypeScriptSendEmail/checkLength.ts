86811e12b12525637b96afed77e51e7507cd4dfc
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-05-16 12:43:13 +0330

    Feat(TypeScriptSendEmail): check status inputs
    These functions will announce the status of the user by checking the different states of an input specified to the user.

--- TypeScriptSendEmail/checkLength.ts
+++ TypeScriptSendEmail/checkLength.ts
@@ -1,11 +1,13 @@
+import success from "./helpers/checkedValidation";
+import failed from "./helpers/failedValidation";
+
 // check length
-function checkLength(input: any): void{
-    if (input.value.length > 0) {
-      input.style.border = "3px solid green";
-    } else {
-      input.style.border = "3px solid red";
-    }
+function checkLength(input: any): void {
+  if (input.value.length > 0) {
+    success(input);
+  } else {
+    failed(input);
   }
-  
-  
-  export default checkLength
+}
+
+export default checkLength;
