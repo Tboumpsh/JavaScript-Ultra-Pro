eb9510cfd699dd36eb6278e90f974936bebd2a06
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-05-22 21:05:44 +0330

    Feat(musicPlayer):Add components to this file
    Setting the components of this design in the main file

--- musicPlayer/musicPlayer.js
+++ musicPlayer/musicPlayer.js
@@ -1,26 +1,50 @@
 import domGenerator from "dom-generator";
 import "./index.scss";
 
+import buttonGenerator from "./component/buttonComponent/buttonComponent";
+import cardGenerator from "./component/cardComponent/cardComponent";
+
 function musicPlayerPlatform() {
   let musicPlayer = domGenerator({
     tag: "div",
     attributes: { id: "appPlayer" },
     children: [
-      
       {
-        tag: "div",
-        attributes: { id: "CardMusicPlayer" },
+        tag: cardGenerator({
+          size: "medium",
+          status: "primary",
+          className: "CardMusicPlayer",
+          upImage:
+            "../public/images/musicPlayer/danny-howe-bn-D2bCvpik-unsplash.jpg",
+        }),
         children: [
-          {
-            tag: "div",
-            attributes: { id: "musicImage" },
-          },
           {
             tag: "div",
             attributes: { id: "musicController" },
             children: [
               {
-                tag: "button",
+                tag: buttonGenerator({
+                  content: "play",
+                  size: "medium",
+                  statues: "primary",
+                  // eventListeners: { click: sendEmails },
+                }),
+              },
+              {
+                tag: buttonGenerator({
+                  content: "play",
+                  size: "large",
+                  statues: "primary",
+                  // eventListeners: { click: sendEmails },
+                }),
+              },
+              {
+                tag: buttonGenerator({
+                  content: "play",
+                  size: "medium",
+                  statues: "primary",
+                  // eventListeners: { click: sendEmails },
+                }),
               },
             ],
           },
