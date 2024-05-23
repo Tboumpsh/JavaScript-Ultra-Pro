64d9bef86b1f36bec18350c877c34e7b98c2bfec
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-05-23 08:13:21 +0330

    Feat(rockPaperScissors):basic structure
    Setting the basic structure of the game.

--- RockPaperScissors/RockPaperScissors.js
+++ RockPaperScissors/RockPaperScissors.js
@@ -1,13 +1,57 @@
 import domGenerator from "dom-generator";
 import "./index.scss";
 
-function RockPaperScissorsGame() {
-  let RockPaperScissors = domGenerator({
+import buttonRockPaperScissorsComponent from "./component/buttonsComponent";
+
+function rockPaperScissorsGame() {
+  let rockPaperScissors = domGenerator({
     tag: "div",
-    attributes: { id: "appPlayer" },
-    children: [],
+    attributes: { id: "backgroundGame" },
+    children: [
+        {
+tag:'section',
+attributes:{id:'headerSection'},
+children:[
+    {
+        tag:'h1',
+        properties: { textContent: "Scores:" },
+        attributes:{id:'Scores'}
+    }
+]
+        },
+      {
+        tag: "div",
+        attributes: { id: "buttonsGroup" },
+        children: [
+          {
+            tag: buttonRockPaperScissorsComponent({
+              content: "play",
+              size: "medium",
+              statues: "possible",
+              // eventListeners: { click: sendEmails },
+            }),
+          },
+          {
+            tag: buttonRockPaperScissorsComponent({
+              content: "play",
+              size: "medium",
+              statues: "possible",
+              // eventListeners: { click: sendEmails },
+            }),
+          },
+          {
+            tag: buttonRockPaperScissorsComponent({
+              content: "play",
+              size: "medium",
+              statues: "possible",
+              // eventListeners: { click: sendEmails },
+            }),
+          },
+        ],
+      },
+    ],
   });
 
-  document.body.append(RockPaperScissors);
+  document.body.append(rockPaperScissors);
 }
-export default RockPaperScissorsGame;
+export default rockPaperScissorsGame;
