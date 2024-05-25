f2cca88bbf8780bad9d865f208ae1129dfb4f76d
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-05-25 13:30:42 +0330

    Fixed(personalBlog):base structure UI
    create basic structure UI of personalBlog

---
+++ prsonalBlog/personalBlog.js
@@ -0,0 +1,39 @@
+import domGenerator from "dom-generator";
+import "./index.scss";
+
+import settingCards from "./settingCard";
+
+function personalBlog() {
+  let blogs = domGenerator({
+    tag: "div",
+    attributes: { id: "blogBackground" },
+    children: [
+      {
+        tag: "div",
+        attributes: { id: "headerBlog" },
+        children: [
+          {
+            tag: "div",
+            attributes: { id: "profile" },
+          },
+          {
+            tag: "h2",
+            attributes: { id: "logoName" },
+            properties: { textContent: "personal Blogs🌟" },
+          },
+        ],
+      },
+      {
+        tag: "button",
+        attributes: { id: "floatButtons" },
+        properties: { textContent: "+" },
+        eventListeners: { click: showSettingForm },
+      },
+    ],
+  });
+  function showSettingForm() {
+    settingCards();
+  }
+  document.body.append(blogs);
+}
+export default personalBlog;
