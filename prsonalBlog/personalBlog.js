8373cb448956ed8993df1e7755ecb3312460b577
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-05-27 11:42:38 +0330

    Feat(personalBlog):personal blog comment
    Complete and comprehensive explanations about how the functions work and the input parameters of each function by jsdoc

--- prsonalBlog/personalBlog.js
+++ prsonalBlog/personalBlog.js
@@ -3,6 +3,19 @@ import "./index.scss";
 
 import settingCards from "./settingCard";
 
+/**
+ * Initializes and renders a personal blog page structure.
+ *
+ * This function uses a DOM generator to create the structure of a personal blog page, which includes a header with a profile section,
+ * a logo, a card section for blog posts, and a floating button to add new posts. The floating button triggers the `showSettingForm`
+ * function, which calls the `settingCards` function.
+ *
+ * @function personalBlog
+ * @returns {void}
+ * @example
+ * // Call the function to initialize and render the personal blog
+ * personalBlog();
+ */
 function personalBlog() {
   let blogs = domGenerator({
     tag: "div",
@@ -35,7 +48,15 @@ function personalBlog() {
       },
     ],
   });
-
+/**
+   * Displays the settings form for creating a new blog post.
+   *
+   * This function is called when the floating button is clicked. It invokes the `settingCards` function to display
+   * the form for adding a new blog post.
+   *
+   * @function showSettingForm
+   * @returns {void}
+   */
   function showSettingForm() {
     settingCards();
   }
