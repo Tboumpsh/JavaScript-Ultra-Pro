0257a76d18149aa603348b808547b3d77ab91d4a
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-05-27 11:42:06 +0330

    Feat(personalBlog):blog list comment
    Complete and comprehensive explanations about how the functions work and the input parameters of each function by jsdoc

--- prsonalBlog/blogList.js
+++ prsonalBlog/blogList.js
@@ -1,6 +1,20 @@
 import domGenerator from "dom-generator";
 import "./index.scss";
 
+/**
+ * Creates a blog post and appends it to the card section.
+ *
+ * The function generates a blog post element with a title and body using a DOM generator and appends it
+ * to an existing section in the document identified by `cardSection`.
+ *
+ * @function BlogList
+ * @param {string} postTitle - The title of the blog post.
+ * @param {string} postBody - The body content of the blog post.
+ * @returns {void}
+ * @example
+ * // Call the function to create and append a blog post
+ * BlogList("My First Post", "This is the content of my first post.");
+ */
 function BlogList(postTitle, postBody) {
   let lists = domGenerator({
     tag: "div",
