84b205faab9372a1c4d79f5f3de6e15b48478909
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-06-05 16:42:22 +0330

    Feat(carInsurance): data structure
    A complete data from car information stored

---
+++ CarInsurance/src/data/objectInformationCars.js
@@ -0,0 +1,18 @@
+let typeOfCars = {
+  Benz: 2.1,
+  Audi: 1.2,
+  Bentley: 2.4,
+  BMW: 2.5,
+  Ferrari: 2.8,
+  Bugatti: 2.9,
+  Jaguar: 2.1,
+  Lamborghini: 3.1,
+  LandRover: 3.2,
+  Rolls_Royce: 4.5,
+  McLarne: 3.3,
+  Porsche: 3.4,
+  MorrisMini_Minor: 3.5,
+  MercedesBenz: 3.6,
+};
+
+export default typeOfCars;
